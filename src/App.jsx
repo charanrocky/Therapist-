d10ea39543208fa3b63be3c613dbe831db5c45a3
charanrocky <charanrocky441@gmail.com>
2025-07-03 17:47:09 +0530

    Initial

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ function App() {
   return (
     <div className="font-sans text-gray-800">
       <Hero />
-      <About />
       <StatsSection />
       <Services />
+      <About />
+
       <SessionFees />
       <FAQ />
       <Contact />
